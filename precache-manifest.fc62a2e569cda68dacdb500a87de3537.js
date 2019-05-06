589d34d1f2475310413fdc5d0526605fca2a4db3
Erdi Uçar <ucarerdi@gmail.com>
2019-05-06 23:20:24 +0300

    Updates

---
+++ precache-manifest.fc62a2e569cda68dacdb500a87de3537.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc14bbf63886260c2f209b54ef698b6",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f323534df1ffd64bc8e4",
+    "url": "/my-app/static/css/main.13ddf5fb.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/my-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f323534df1ffd64bc8e4",
+    "url": "/my-app/static/js/main.f1a2cc6e.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
